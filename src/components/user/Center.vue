0ea5ff2484656d7695c74905b0e8cb9ac959a3ea
youzi177 <youzi177@126.com>
2026-07-25 17:19:47 +0800

    用户中心基础样式

--- src/components/user/Center.vue
+++ src/components/user/Center.vue
@@ -1,7 +1,167 @@
 <template>
-  <div>Center</div>
+  <div class="panel pd20 main">
+    <div class="msg">Hi,Admin,你已经是我们的正式会员！</div>
+    <div class="layui-row layui-col-space20">
+      <!-- 会员信息和签到 -->
+      <div class="layui-col-md6">
+        <div class="panel border">
+          <div class="title">我的会员信息</div>
+          <div class="content">
+            <p>积分经验值：60</p>
+            <p>您当前为:非VIP</p>
+          </div>
+        </div>
+      </div>
+      <div class="layui-col-md6">
+        <Sign class="border"></Sign>
+      </div>
+      <!-- 快捷方式 -->
+      <div class="layui-col-md12">
+        <div class="panpel border">
+          <div class="title">快捷方式</div>
+          <div class="content">
+            <ul class="layui-row layui-col-space10">
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+              <li class="layui-col-sm3 layui-col-xs4">
+                <a href="">
+                  <div class="layui-icon layui-icon-set shortcut"></div>
+                  <span>修改密码</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Sign from '../sidebar/Sign.vue'
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+// x768的时候，把panel的margin值为0
+@media screen and (max-width: 768px) {
+  .panel {
+    &.main {
+      margin: 0 !important;
+    }
+  }
+}
+.panel {
+  border-radius: 2px;
+  background-color: #fff;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+  transition: all 0.2s;
+  color: #333;
+  &.main {
+    margin: 0 0 10px 225px;
+  }
+}
+.msg {
+  background-color: #f8f8f8;
+  color: #666;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+}
+.border {
+  border: 1px solid #e6e6e6;
+}
+.pd20 {
+  padding: 20px;
+}
+.title {
+  height: 50px;
+  line-height: 50px;
+  border-bottom: 1px dotted #e9e9e9;
+  font-size: 14px;
+  padding: 0 15px;
+}
+.content {
+  padding: 18px 15px;
+  // height: 50px;
+  line-height: 26px;
+  font-size: 14px;
+  color: #666;
+}
+li {
+  text-align: center;
+  padding: 5px;
+}
+.shortcut {
+  background-color: #2f9688;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  color: #fff;
+  border-radius: 2px;
+  font-size: 30px;
+}
+</style>
--- src/pages/Center.vue
+++ src/pages/Center.vue
@@ -7,6 +7,7 @@
         >
       </li>
     </ul>
+    <RouterView></RouterView>
   </div>
 </template>
 
