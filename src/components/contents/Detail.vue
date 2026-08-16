5f68e2bd1ff2d2557c478ae7f7107965980441f7
youzi177 <youzi177@126.com>
2026-08-16 17:37:21 +0800

    新增详情页样式

--- src/components/contents/Detail.vue
+++ src/components/contents/Detail.vue
@@ -1,7 +1,235 @@
 <template>
-  <div></div>
+  <div class="layui-container">
+    <Panel></Panel>
+    <div class="layui-row layui-col-space15">
+      <div class="layui-col-md8 content detail">
+        <div class="fly-panel detail-box">
+          <h1>geek极客社区，基于 layui 的极简社区页面模版</h1>
+          <div class="fly-detail-info">
+            <!-- <span class="layui-badge">审核中</span> -->
+            <span class="layui-badge layui-bg-green fly-detail-column">动态</span>
+
+            <span class="layui-badge" style="background-color: #999">未结</span>
+            <!-- <span class="layui-badge" style="background-color: #5FB878;">已结</span> -->
+
+            <span class="layui-badge layui-bg-black">置顶</span>
+            <span class="layui-badge layui-bg-red">精帖</span>
+
+            <div class="fly-admin-box" data-id="123">
+              <span class="layui-btn layui-btn-xs jie-admin" type="del">删除</span>
+
+              <span class="layui-btn layui-btn-xs jie-admin" type="set" field="stick" rank="1"
+                >置顶</span
+              >
+              <!-- <span class="layui-btn layui-btn-xs jie-admin" type="set" field="stick" rank="0" style="background-color:#ccc;">取消置顶</span> -->
+
+              <span class="layui-btn layui-btn-xs jie-admin" type="set" field="status" rank="1"
+                >加精</span
+              >
+              <!-- <span class="layui-btn layui-btn-xs jie-admin" type="set" field="status" rank="0" style="background-color:#ccc;">取消加精</span> -->
+            </div>
+            <span class="fly-list-nums">
+              <a href="#comment"><i class="iconfont" title="回答">&#xe60c;</i> 66</a>
+              <i class="iconfont" title="人气">&#xe60b;</i> 99999
+            </span>
+          </div>
+          <!-- 收藏、作者信息 -->
+          <div class="detail-about">
+            <a class="fly-avatar" href="../user/home.html">
+              <img
+                src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"
+                alt="贤心"
+              />
+            </a>
+            <div class="fly-detail-user">
+              <a href="../user/home.html" class="fly-link">
+                <cite>贤心</cite>
+                <i class="iconfont icon-renzheng" title="认证信息：{{ rows.user.approve }}"></i>
+                <i class="layui-badge fly-badge-vip">VIP3</i>
+              </a>
+              <span>2017-11-30</span>
+            </div>
+            <div class="detail-hits" id="LAY_jieAdmin" data-id="123">
+              <span style="padding-right: 10px; color: #ff7200">悬赏：60积分</span>
+            </div>
+          </div>
+          <div class="layui-btn-container fly-detail-admin pt-1">
+            <a href="" class="layui-btn layui-btn-sm jie-admin">编辑</a>
+            <a href="" class="layui-btn layui-btn-sm jie-admin jieda-admin-colllect">收藏</a>
+          </div>
+          <div class="detail-body photos">这是贴子内容</div>
+        </div>
+        <!-- 回帖相关内容 -->
+        <div class="fly-panel detail-box" id="flyReply">
+          <fieldset class="layui-elem-field layui-field-title" style="text-align: center">
+            <legend>回帖</legend>
+          </fieldset>
+
+          <ul class="jieda" id="jieda">
+            <li data-id="111" class="jieda-daan">
+              <a name="item-1111111111"></a>
+              <div class="detail-about detail-about-reply">
+                <a class="fly-avatar" href="">
+                  <img
+                    src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"
+                    alt=" "
+                  />
+                </a>
+                <div class="fly-detail-user">
+                  <a href="" class="fly-link">
+                    <cite>贤心</cite>
+                    <i class="iconfont icon-renzheng" title="认证信息：XXX"></i>
+                    <i class="layui-badge fly-badge-vip">VIP3</i>
+                  </a>
+
+                  <span>(楼主)</span>
+                  <!--
+                <span style="color:#5FB878">(管理员)</span>
+                <span style="color:#FF9E3F">（社区之光）</span>
+                <span style="color:#999">（该号已被封）</span>
+                --></div>
+
+                <div class="detail-hits">
+                  <span>2017-11-30</span>
+                </div>
+
+                <i class="iconfont icon-caina" title="最佳答案"></i>
+              </div>
+              <div class="detail-body jieda-body photos">
+                <p>香菇那个蓝瘦，这是一条被采纳的回帖</p>
+              </div>
+              <div class="jieda-reply">
+                <span class="jieda-zan zanok" type="zan">
+                  <i class="iconfont icon-zan"></i>
+                  <em>66</em>
+                </span>
+                <span type="reply">
+                  <i class="iconfont icon-svgmoban53"></i>
+                  回复
+                </span>
+                <div class="jieda-admin">
+                  <span type="edit">编辑</span>
+                  <span type="del">删除</span>
+                  <!-- <span class="jieda-accept" type="accept">采纳</span> -->
+                </div>
+              </div>
+            </li>
+
+            <li data-id="111">
+              <a name="item-1111111111"></a>
+              <div class="detail-about detail-about-reply">
+                <a class="fly-avatar" href="">
+                  <img
+                    src="https://tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg"
+                    alt=" "
+                  />
+                </a>
+                <div class="fly-detail-user">
+                  <a href="" class="fly-link">
+                    <cite>贤心</cite>
+                  </a>
+                </div>
+                <div class="detail-hits">
+                  <span>2017-11-30</span>
+                </div>
+              </div>
+              <div class="detail-body jieda-body photos">
+                <p>蓝瘦那个香菇，这是一条没被采纳的回帖</p>
+              </div>
+              <div class="jieda-reply">
+                <span class="jieda-zan" type="zan">
+                  <i class="iconfont icon-zan"></i>
+                  <em>0</em>
+                </span>
+                <span type="reply">
+                  <i class="iconfont icon-svgmoban53"></i>
+                  回复
+                </span>
+                <div class="jieda-admin">
+                  <span type="edit">编辑</span>
+                  <span type="del">删除</span>
+                  <span class="jieda-accept" type="accept">采纳</span>
+                </div>
+              </div>
+            </li>
+
+            <!-- 无数据时 -->
+            <!-- <li class="fly-none">消灭零回复</li> -->
+          </ul>
+
+          <div class="layui-form layui-form-pane">
+            <Form @submit="submit" v-slot="{ errors }">
+              <Editor @changeContent="add" :initContent="state1.content"></Editor>
+              <div class="layui-form-item">
+                <label for="L_vercode" class="layui-form-label">验证码</label>
+                <div class="layui-input-inline">
+                  <Field
+                    type="text"
+                    as="input"
+                    rules="required|length:4"
+                    name="code"
+                    placeholder="请输入验证码"
+                    class="layui-input"
+                    v-model="code"
+                  />
+                </div>
+                <div>
+                  <span class="svg" style="color: #c00" @click="_getCode" v-html="svg"></span>
+                </div>
+                <div class="layui-form-mid">
+                  <span style="color: #c00">{{ errors.code }}</span>
+                </div>
+              </div>
+              <div class="layui-form-item">
+                <!-- <input type="hidden" name="jid" value="123" /> -->
+                <button class="layui-btn" type="submit">提交回复</button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+      <div class="layui-col-md4">
+        <HotList></HotList>
+        <Ads></Ads>
+        <Links></Links>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted, reactive, toRefs } from 'vue'
+import Panel from '../Panel.vue'
+import Ads from '../sidebar/Ads.vue'
+import HotList from '../sidebar/HotList.vue'
+import Links from '../sidebar/Links.vue'
+import Editor from '@/components/modules/editor/Index.vue'
+import Uselogin from '@/hooks/Uselogin'
+import { Field, Form } from 'vee-validate'
+//封装函数
+const { state, _getCode, setid } = Uselogin()
+const { code, svg } = toRefs(state)
+const state1 = reactive({
+  content: '',
+})
 
-<style></style>
+onMounted(() => {
+  setid()
+  _getCode()
+})
+const submit = () => {}
+const add = () => {}
+</script>
+
+<style lang="scss" scoped>
+.fly-detail-admin {
+  text-align: right;
+  border-top: 1px dotted #eaeaea;
+  background: #f8f8f8;
+}
+.fly-detail-info {
+  span {
+    margin-right: 5px;
+  }
+}
+</style>
